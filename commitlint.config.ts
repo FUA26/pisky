aef05f99f01fadd3bfe4158025054b4e9f3e24ae
fatihua <fatihua@gmail.com>
2026-03-28 08:25:22 +0700

    chore: configure Lefthook, Commitlint, and VSCode settings

---
+++ commitlint.config.ts
@@ -0,0 +1,27 @@
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "subject-case": [0],
+  },
+};
+
+export default Configuration;
